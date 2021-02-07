b98f114331e160f034df0c20c181f407cd980628
Rishabh Chaurasia <chaurasia.rishabh7@gmail.com>
2021-02-08 00:00:41 +0300

    Starting the reinforcement Learning Part

---
+++ rl.py
@@ -0,0 +1,13 @@
+import numpy as numpy
+import pandas as pd
+
+
+import keras
+from keras.models import Sequential
+from keras.models import load_model
+from keras.layers import Dense
+from keras.optimizers import Adam
+import math
+import numpy as np
+import random
+from collections import deque
